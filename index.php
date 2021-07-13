a3fb2070efaa37dba8558fe8428a92ee78b773d6
andresdeveloper91 <sergiosorio_91@hotmail.com>
2021-07-13 01:20:08 -0500

    corrigiendo titulo

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-wiidth, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <tittle>Blog de Videojuegos</tittle>
+        <title>Blog de Videojuegos</title>
     </head>
     <body>
         <!-- ENCABEZADO-->
