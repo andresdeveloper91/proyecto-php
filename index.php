910e143ae1e6ded0163a1d09969f8653ff9cb04b
andresdeveloper91 <sergiosorio_91@hotmail.com>
2021-07-13 01:10:44 -0500

    se añade la maqueta y el encabezado

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-wiidth, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <tittle>web</tittle>
+    </head>
+    <body>
+        <!-- ENCABEZADO-->
+        <header>
+           <div id="logo">
+                <a href="index.php">
+                    Blog de videojuegos
+                </a>
+            </div> 
+        </header>
+        <!-- MENU-->
+        <!-- SIDEBAR-->
+        <!-- CAJA PRINCIPAL-->
+        <!-- PIE DE PAGINA-->
+
+    </body>
+</html>
